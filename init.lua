957af7345143d2ce0d60868aa9021e7be9ecb842
Harry Foster <harrywfoster10@gmail.com>
2026-02-21 18:43:54 +0000

    added keymappings

--- init.lua
+++ init.lua
@@ -1,4 +1,15 @@
-require('options')
-require('keymaps')
-require('plugins')
-require('lsp')
+-- 1. Load options first (so they are ready for plugins)
+require("options")
+
+
+-- 4. Load Plugins (this will now auto-import everything in plugin-configs)
+require("plugins")
+
+-- 2. Load keymaps
+require("keymaps")
+
+-- 3. Load LSP autocmds
+require("lsp")
+
+-- 5. Load colorscheme (optional, or let theme-hub handle it)
+require("colorscheme")
